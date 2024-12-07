28437f9a4f33e1d1b31e0e1df13a41901d15b239
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2024-12-07 23:44:26 +0500

    update

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -4,8 +4,8 @@ import { motion } from "framer-motion";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      initial={{ y: 80, opacity: 0, x: -200 }}
-      animate={{ y: 0, opacity: 1, x: 0 }}
+      initial={{ y: 80, opacity: 0, x: -200, scale: 0.8 }}
+      animate={{ y: 0, opacity: 1, x: 0, scale: 1 }}
       transition={{ type: "easeInOut", duration: 0.8 }}
     >
       {children}
