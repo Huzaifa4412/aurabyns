abd843cbebb88755e8acef5b060a0fd7913a5d83
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-01-01 01:27:54 +0500

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,18 @@ import FeatureSection from "@/components/FeaturesSection/page";
 import HeroSection from "@/components/HeroSection/page";
 import React from "react";
 import Slider from "../components/Slider/page";
-import ComingSoon from "@/components/ComingSoon/ComingSoon";
+import Timer from "@/components/ComingSoon/Timer";
+// import ComingSoon from "@/components/ComingSoon/ComingSoon";
 
 const page = () => {
   return (
     <>
       <Slider />
-      <ComingSoon />
+      {/* <ComingSoon /> */}
+      <div className="flex items-center justify-center">
+        <Timer launchDate="2025-02-01T02:00:00" />
+      </div>
+
       <HeroSection />
       <FeaturesCategory />
       <FeatureSection />
