92c1c7f7ba3a52d976eec9f2890606f9e25c46fa
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-01-14 17:16:18 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: 'export', // Enable static export
-  // trailingSlash: true, // Adds a trailing slash to all routes (important for static hosting)
-  // images: {
-  //   unoptimized: true, // Disable image optimization for static export
-  // },
   eslint: {
     ignoreDuringBuilds: true,
   }
