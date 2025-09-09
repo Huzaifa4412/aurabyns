2a8b8149f679f488318c9558b6fb0323bc896254
Huzaifa Mukhtar <mukhtarhuzaifa436@gmail.com>
2025-09-09 21:24:01 +0500

    update

--- src/components/Whatsapp/WhatsappIcon.tsx
+++ src/components/Whatsapp/WhatsappIcon.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 function WhatsappIcon() {
-  return (
-    <div className="fixed left-5 z-[999] bottom-[5%]">
-      <a href="https://api.whatsapp.com/message/WNUA7YUX5C6RI1?autoload=1&app_absent=0">
-        <img
-          src={"whatsappIcon.svg"}
-          alt="Whatsapp Icon"
-          width={80}
-          height={80}
-          className="w-[45px] lg:w-[80px]"
-        />
-      </a>
-    </div>
-  );
+    return (
+        <div className="fixed left-5 z-[999] bottom-[5%]">
+            <a href="https://wa.link/h20dlb" target="_blank">
+                <img
+                    src={"whatsappIcon.svg"}
+                    alt="Whatsapp Icon"
+                    width={80}
+                    height={80}
+                    className="w-[45px] lg:w-[80px]"
+                />
+            </a>
+        </div>
+    );
 }
 
 export default WhatsappIcon;
